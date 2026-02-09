8c75a56fea5c8e6c6492ef8d28e40e9298257807
workdevapi-byte <workdevapi@gmail.com>
2026-02-09 15:20:41 +0530

    Update vite.config.js
    Added hosted backend endpoint

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://workdevapi-byte-the-reserve-backend-pezh56wn4.vercel.app',
         changeOrigin: true,
       },
     },
   },
 });
+
